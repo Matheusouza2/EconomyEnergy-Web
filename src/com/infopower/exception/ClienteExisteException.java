ee93562f28497d903e3b6e2603cdb44c53304b14
Matheus Souza <Matheus Souza>
2016-05-08 21:09:22 -0300

    Adição da requisição ajax. Graficos e cadastros relacionados a arduino funcionais

--- src/com/infopower/exception/ClienteExisteException.java
+++
@@ -1,9 +0,0 @@
-package com.infopower.exception;
-
-public class ClienteExisteException extends Exception {
-	
-	public  ClienteExisteException(){
-		super("ID NÃO ENCONTRADO!");
-	}
- 
-}
--- src/com/infopower/exception/SQLException.java
+++
@@ -1,9 +0,0 @@
-package com.infopower.exception;
-
-public class SQLException extends Exception {
-	
-	public  SQLException(){
-		super("NÃO ENCONTRADO!");
-	}
-
-}
