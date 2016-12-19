8fbb34822911940d6dc9450351da603a8975ea86
Matheus Souza <Matheus Souza>
2016-12-19 10:26:07 -0200

    Versão final do projeto

--- InfoPowerV1.8-Arduino/InfoPowerV1.8-Arduino.ino
+++ InfoPowerV1.8-Arduino/InfoPowerV1.8-Arduino.ino
@@ -1,10 +1,10 @@
 /*
  * Projeto: Economy Energy
  * Autor: Grupo InfoPower
- * Data: 03/05/2016
- * Versão: v1.7
- * Descrição: O Arduino recebe uma requisição HTTP e retorna um JSON que é tratado no HTML com Javascript para exibir na tela.
- * Versão 1.7 trás primeiro teste de acionamento de led com relê
+ * Data: 20/11/2016
+ * Versão: v1.8
+ * Descrição: O Arduino recebe uma requisição HTTP e retorna um JSON que é tratado no HTML com Javascript para exibir na tela, acrescentando a opção de apagar luzaes e desligar tomadas.
+ * Versão 1.8 trás primeiro teste de acionamento de led com relê
  */
     
 #include <SPI.h>                
@@ -24,7 +24,8 @@ EnergyMonitor sensor;                   // Cria a instancia do sensor
 double ampers = 0;
 double potencia = 0;
 double kw = 0;
-int porta_rele1 = 7;
+int pin = 7;
+String readString;
 
 void setup(){
   Serial.begin(9600); // inicia o arduino
@@ -35,14 +36,10 @@ void setup(){
   
   sensor.current(1, 154.4);        //Define o pino "1" como o pino que vai receber os dados e 154.4 é a calibração do sensor 100A/50mA para leitura mais aproximada possivel
 
-  pinMode(porta_rele1, OUTPUT); 
+  pinMode(pin, OUTPUT); 
 }
 
 void loop(){
-  digitalWrite(porta_rele1, LOW);  //Liga rele 1
-  delay(2000);
-  digitalWrite(porta_rele1, HIGH); //Desliga rele 1
-  
   EthernetClient cliente = servidor.available(); // define um cliente para o uso do server
   
    ampers = sensor.calcIrms(1480);  //Calculo padrão para leitura de irms(Amper)
@@ -50,6 +47,7 @@ void loop(){
    kw = (((potencia)/1000.0) * 1.0/3600.0); //Calculo do kw
   
   enviarServlet(kw); // chama o metodo de envio de dados pro servlet
+  printPage(); //Chama o metodo para ligar e desligar o relê
   
   if(cliente){
     boolean continua = true;
@@ -91,14 +89,14 @@ void loop(){
 void enviarServlet(double kw){
  EthernetClient clienteServlet; //instancia um cliente para envio de dados via requisição GET para o servlet
   
-  clienteServlet.connect("192.168.0.110",9091);// define o ip e porta ao qual vai ser enviada a requisição
+  clienteServlet.connect("192.168.0.100",9090);// define o ip e porta ao qual vai ser enviada a requisição
   
   if (clienteServlet.connected()) { //se o server encontrar o ip e a porta livre ele envia a requisição.
     clienteServlet.print("GET /InfoPower/ArduinoServlet?");
     clienteServlet.print("kw=");
     clienteServlet.print(kw,DEC);
     clienteServlet.println(" HTTP/1.1");
-    clienteServlet.println("Host: 192.168.0.110");
+    clienteServlet.println("Host: 192.168.0.100");
     clienteServlet.println();
     
 
@@ -111,5 +109,54 @@ void enviarServlet(double kw){
     Serial.println("Dados enviados!");
   } else {
     Serial.println("Falha na conexão.");
+  }
+}
+
+void printPage(){
+  EthernetServer server(9092);
+ EthernetClient cliente = server.available();
+  if (cliente) {
+    while (cliente.connected()) {
+      if (cliente.available()) {
+        char c = cliente.read();
+  
+        if (readString.length() < 100) {
+          readString += c;             
+        }
+ 
+        if (c == '\n') {
+          cliente.println("HTTP/1.1 200 OK");
+          cliente.println("Content-Type: text/html");
+          cliente.println();
+           
+          cliente.println("<HTML>");
+          cliente.println("<BODY>");
+          cliente.println("<H1>Acende LED</H1>");
+          cliente.println("<hr />");
+          cliente.println("<br />");
+           
+          cliente.println("<a href=\"/?ledon\">Ligar o led</a>");
+          cliente.println("<a href=\"/?ledoff\">Desligar o led</a><br />");   
+           
+          cliente.println("</BODY>");
+          cliente.println("</HTML>");
+           
+          delay(1);
+          cliente.stop();
+           
+          if(readString.indexOf("?ledon") > 0)
+          {
+            digitalWrite(pin, LOW);
+          }
+          else {
+            if(readString.indexOf("?ledoff") > 0)
+            {
+              digitalWrite(pin, HIGH);
+            }
+          }
+          readString="";    
+        }
+      }
+    }
   }
 }
